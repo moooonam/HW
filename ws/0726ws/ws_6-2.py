332e8af7dbab9f317eb119fb755cfe77fccfc093
moooonam <anska77@naver.com>
2022-07-26 17:17:31 +0900

    up

--- ws/0726ws/ws_6-2.py
+++ ws/0726ws/ws_6-2.py
@@ -1,5 +1,11 @@
 grain_lst = [('고구마',3000), ('감자',2000), ('옥수수',4500),('토란',1300)]
-mylist =[]
-for i in grain_lst:
-    if i
 
+
+a = 0
+for i in grain_lst:
+    if a < int(i[1]):
+        a = int(i[1])
+for j in grain_lst:
+    if a in j:
+        print(j[0]) 
+    

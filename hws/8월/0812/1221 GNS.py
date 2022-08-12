80ef2fa36c1477cd78fd399568e9f8606cf94438
moooonam <anska77@naver.com>
2022-08-12 17:47:26 +0900

    up

---
+++ hws/8월/0812/1221 GNS.py
@@ -0,0 +1,7 @@
+T=int(input())
+for tc in range(1,T+1):
+    N, L =input().split()
+    L=int(L)
+    vin=[0]*10 # 이 리스트에 갯수 넣어주고 빈 리스트에 차례대로 넣어주자
+    arr1=["ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"]
+    

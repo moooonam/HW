6de90faa70955502737695f6a7e80d08d25910fd
moooonam <anska77@naver.com>
2022-11-10 17:56:39 +0900

    ✈  up

---
+++ hws/11월/1110/youtube-project/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <h1>My First Youtube Project</h1>
+    <SearchTitle
+    @give-to-title="appGetTitle"
+    />
+  </div>
+</template>
+
+<script>
+import SearchTitle from '@/components/SearchTitle'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      
+    }
+  },
+  components: {
+    SearchTitle,
+  },
+  methods:{
+    appGetTitle(inputData){
+      console.log(inputData)
+
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hws/11월/1110/youtube-project/src/components/SearchTitle.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+		검색:
+		<input type="text"
+		v-model="inputData" @keyup.enter="giveToTitle">
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'SearchTitle',
+	data (){
+		return{
+			inputData:null,
+		}
+	},
+	methods:{
+		giveToTitle(){
+			this.$emit('give-to-title', this.inputData)
+			
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
